f18d0e7c9f4b657ceb149fa13d5eee4fc710ce15
Agustin Aused <agus.aused@gmail.com>
2025-06-16 16:32:38 -0300

    refactor: simplificar configuración de seguridad usando referencias a AbstractHttpConfigurer

--- src/main/java/com/pds/sportsmanager/config/SecurityConfig.java
+++ src/main/java/com/pds/sportsmanager/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,13 +21,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll() 
+                .anyRequest().permitAll()
             )
             .headers(headers -> headers.frameOptions().disable())
-            .httpBasic(httpBasic -> httpBasic.disable()) 
-            .formLogin(form -> form.disable()); 
+            .httpBasic(httpBasic -> httpBasic.disable())
+            .formLogin(form -> form.disable());
 
         return http.build();
     }
