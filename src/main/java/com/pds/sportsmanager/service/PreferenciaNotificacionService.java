8780484be67ab88b8597c600fc58a9bab5c1fe2e
Agustin Aused <agus.aused@gmail.com>
2025-06-26 14:59:28 -0300

    feat: extend PreferenciaNotificacionService to inherit JpaRepository for improved data access

--- src/main/java/com/pds/sportsmanager/service/PreferenciaNotificacionService.java
+++ src/main/java/com/pds/sportsmanager/service/PreferenciaNotificacionService.java
@@ -1,10 +1,11 @@
 package com.pds.sportsmanager.service;
 
 import com.pds.sportsmanager.model.entity.PreferenciaNotificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PreferenciaNotificacionService {
+public interface PreferenciaNotificacionService extends JpaRepository<PreferenciaNotificacion, Long> {
     Optional<PreferenciaNotificacion> obtenerPorUsuarioId(Long usuarioId);
 
     Optional<PreferenciaNotificacion> obtenerPorEmail(String email);
