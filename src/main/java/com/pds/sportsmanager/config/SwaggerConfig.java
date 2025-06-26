4a742f5a4501a3b741ab842a48617758eb565d73
Agustin Aused <agus.aused@gmail.com>
2025-06-25 22:43:43 -0300

    feat: add Swagger configuration for API documentation and update security settings

---
+++ src/main/java/com/pds/sportsmanager/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.pds.sportsmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+            .info(new Info().title("Sports Manager API").version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
